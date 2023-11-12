8db9756aeb310ebd0ebecfe1491b4e1fecbc1bd9
kingssik <hyunsikkang25@gmail.com>
2023-11-12 12:56:27 +0900

    게시물 작성시 날짜 및 시간 저장

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,6 +1,5 @@
 package com.KoreaIT.java.AM;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -26,12 +25,13 @@ public class Main {
       if (cmd.equals("article write")) {
         int id = lastArticleId + 1;
         lastArticleId = id;
+        String regDate = Util.getNowDateStr();
         System.out.print("제목 : ");
         String title = sc.nextLine();
         System.out.print("내용 : ");
         String body = sc.nextLine();
 
-        Article article = new Article(id, title, body);
+        Article article = new Article(id, regDate, title, body);
         articles.add(article);
         System.out.printf("%d번 글이 생성되었습니다\n", id);
 
@@ -84,7 +84,7 @@ public class Main {
           continue;
         }
         System.out.printf("번호 : %d\n", foundArticle.id);
-        System.out.printf("날짜 : 2023-11-12 12:12:12\n");
+        System.out.printf("날짜 : %s\n", foundArticle.regDate);
         System.out.printf("제목 : %s\n", foundArticle.title);
         System.out.printf("내용 : %s\n", foundArticle.body);
 
@@ -100,11 +100,13 @@ public class Main {
 
 class Article {
   int id;
+  String regDate;
   String title;
   String body;
 
-  public Article(int id, String title, String body) {
+  public Article(int id, String regDate, String title, String body) {
     this.id = id;
+    this.regDate = regDate;
     this.title = title;
     this.body = body;
   }
