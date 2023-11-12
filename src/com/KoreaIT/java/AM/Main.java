f460c029fc527a02b793a24fc00463ea73ddf779
kingssik <hyunsikkang25@gmail.com>
2023-11-12 13:04:28 +0900

    게시물 수정 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -47,7 +47,7 @@ public class Main {
         }
       } else if (cmd.startsWith("article delete ")) {
         String[] cmdBits = cmd.split(" ");
-        int id = Integer.parseInt(cmdBits[2]);  // "2" -> 2
+        int id = Integer.parseInt(cmdBits[2]);
 
         int foundIdx = -1;
         for (int i = 0; i < articles.size(); i++) {
@@ -68,7 +68,7 @@ public class Main {
 
       } else if (cmd.startsWith("article detail ")) {
         String[] cmdBits = cmd.split(" ");
-        int id = Integer.parseInt(cmdBits[2]);  // "2" -> 2
+        int id = Integer.parseInt(cmdBits[2]);
 
         Article foundArticle = null;
         for (int i = 0; i < articles.size(); i++) {
@@ -87,6 +87,32 @@ public class Main {
         System.out.printf("날짜 : %s\n", foundArticle.regDate);
         System.out.printf("제목 : %s\n", foundArticle.title);
         System.out.printf("내용 : %s\n", foundArticle.body);
+
+      } else if (cmd.startsWith("article modify ")) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[2]);
+
+        Article foundArticle = null;
+        for (int i = 0; i < articles.size(); i++) {
+          Article article = articles.get(i);
+          if (article.id == id) {
+            foundArticle = article;
+            break;
+          }
+        }
+
+        if (foundArticle == null) {
+          System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+          continue;
+        }
+        System.out.print("제목 : ");
+        String title = sc.nextLine();
+        System.out.print("내용 : ");
+        String body = sc.nextLine();
+
+        foundArticle.title = title;
+        foundArticle.body = body;
+        System.out.printf("%d번 게시물이 수정 되었습니다\n", id);
 
       } else {
         System.out.println("존재하지 않는 명령어입니다");
